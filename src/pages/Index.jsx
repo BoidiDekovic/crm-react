bc9d47ee890fd2e2106c0740e94b632bdc54628c
Martin <boidi.martinalejandro@gmail.com>
2023-07-13 03:53:56 -0300

    proyecto con react router dom

---
+++ src/pages/Index.jsx
@@ -0,0 +1,46 @@
+import { useLoaderData } from "react-router-dom";
+import { obtenerClientes } from "../data/Clientes";
+import Cliente from "../components/Cliente";
+
+export function loader(){
+    const clientes = obtenerClientes()
+    return clientes
+}
+
+function Index() {
+
+  const clientes = useLoaderData();
+  
+  return (
+    <>
+      <h1 className="font-black text-4xl text-green-800">Clientes</h1>
+      <p className="mt-3 text-2xl">Administra tus Clientes</p>
+    
+      {clientes.length ? (
+        <table className='w-full bg-white shadow mt-5 table-auto'>
+          <thead className='bg-green-800 text-white'>
+              <tr>
+                  <th className='p-2'>Cliente</th>
+                  <th className='p-2'>Contacto</th>
+                  <th className='p-2'>Acciones</th>
+              </tr>
+            </thead>
+              <tbody>
+                {clientes.map(cliente =>(
+                    <Cliente 
+                      cliente={cliente}
+                      key={cliente.id}
+                     />
+                ))}
+              </tbody>
+
+            
+        </table>
+      ) : (
+        <p className="text-center mt-10">No hay clientes aun</p>
+      )}
+    </>
+  )
+}
+
+export default Index
